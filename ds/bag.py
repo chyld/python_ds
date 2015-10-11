6ebfec75d94cbdad6ec796e8d6087e5a18ed92f8
Chyld Medford <chyld.medford@gmail.com>
2015-10-10 17:47:06 -0700

    bag

---
+++ ds/bag.py
@@ -0,0 +1,12 @@
+class Bag:
+    def __init__(self):
+        self._data = []
+    def add(self, item):
+        self._data.append(item)
+    def is_empty(self):
+        return self.size() == 0
+    def size(self):
+        return len(self._data)
+    def __str__(self):
+        return str(self._data)
+    __repr__ = __str__
---
+++ main.py
@@ -0,0 +1,6 @@
+from ds.bag import *
+
+b = Bag()
+b.add(3)
+
+import pdb; pdb.set_trace()
